80ab109cae03aa3a7ba44c9040e82dc127fa5125
CarlosMena01 <cmena@unal.edu.co>
2023-02-02 10:16:28 -0500

    Python V1.0.0 stable version

--- python/main.py
+++ python/main.py
@@ -3,6 +3,14 @@ from PIL import Image
 import numpy as np
 from scipy.fftpack import fft2, fftshift
 
+from progress.bar import Bar
+
+# Parámetros del programa
+m = 10 # Máximo número de FFTs consecutivas
+
+# Creamos la barra de progreso
+bar = Bar('Procesando:', max = m)
+
 # Abrir imagen
 img = Image.open('../image.jpg')
 pixels = np.array(img)
@@ -10,7 +18,7 @@ pixels = np.array(img)
 # Crear archivo de tiempo
 time_file = open("time.txt", "w")
 
-for n in range(1,100):
+for n in range(1,m + 1):
     # Realizar n transformadas FFT
     start = time.time()
     for i in range(n):
@@ -23,4 +31,9 @@ for n in range(1,100):
 
     # Escribir iteración/tiempo en archivo
     time_file.write(str(n) + "/" + str(elapsed) + "\n")
+
+    # Actualizamos la barra de progreso
+    bar.next()
+
+bar.finish()
 time_file.close()
